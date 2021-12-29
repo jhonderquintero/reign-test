ee260c23cbc46a23f112405e721e869712d71916
Jhonder Quintero <jhonderquintero@gmail.com>
2021-12-29 15:59:56 -0400

    design token

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { HomeContent } from "./templates/HomeTemplate"
 
-export const Home = () => {
+export const Home: React.FC = (): JSX.Element => {
   return (
     <div>
       <HomeContent Header={<div>Header</div>} />
