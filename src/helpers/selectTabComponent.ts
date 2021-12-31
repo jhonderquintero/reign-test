48e9423a2bc44e9d2f496291add29c05b713f496
Jhonder Quintero <jhonderquintero@gmail.com>
2021-12-30 23:41:30 -0400

    jsdoc for selectedTabComponent

--- src/helpers/selectTabComponent.ts
+++ src/helpers/selectTabComponent.ts
@@ -1,5 +1,10 @@
 import { IHomeTabs, tabs, Tabs } from "./../views/Home/definitions";
 
+/**
+ * Return an object with selected tab state (React Component [component] and tab title [tabTitle])
+ * @param tabName 
+ * @returns object: {component, tabName}
+ */
 export function selectTabComponent(tabName: IHomeTabs) {
   const selectedTab: Tabs[] = tabs.filter((obj) => {
     return obj.tabName === tabName;
