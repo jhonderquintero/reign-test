3c8610824bf21487269a6a63bc5e6a0193ea8dc8
Jhonder Quintero <jhonderquintero@gmail.com>
2022-01-02 13:37:47 -0400

    querystring added to path

--- src/atomic/molecules/SelectedCards/SelectedCards.tsx
+++ src/atomic/molecules/SelectedCards/SelectedCards.tsx
@@ -1,9 +1,10 @@
 import { TimeSVG } from "../../../atomic/atoms/Icons/TimeSVG";
 import { fetchPosts } from "../../../helpers/fetchPosts";
-import React from "react";
+import React, { useEffect } from "react";
 import { useQuery } from "react-query";
 import { reactQueryHOC } from "../../../helpers/reactQueryHOC";
 import { InfoCard } from "../InfoCard/InfoCard";
+import { useSearchParams } from "react-router-dom";
 
 export const SelectedCards = reactQueryHOC(
   ({ favoritePosts, setFavoritePosts, selectedDropdown, queryClient }: any) => {
@@ -15,64 +16,77 @@ export const SelectedCards = reactQueryHOC(
       { keepPreviousData: true, staleTime: 5000 }
     );
 
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    useEffect(() => {
+      searchParams.set("query", selectedDropdown);
+      setSearchParams(searchParams);
+    }, [selectedDropdown, searchParams, setSearchParams]);
+
     // Prefetch the next page!
     React.useEffect(() => {
+      searchParams.set("page", String(page + 1));
+      setSearchParams(searchParams);
       queryClient.prefetchQuery(["posts", page], () =>
         fetchPosts(page, selectedDropdown)
       );
-    }, [page, queryClient, selectedDropdown]);
+    }, [page, queryClient, searchParams, selectedDropdown, setSearchParams]);
 
     return (
       <div style={{ marginTop: 30 }}>
         {status === "success" && (
           <div>
-            {data.hits.map(
-              ({
-                story_title,
-                story_url,
-                created_at,
-                comment_text,
-                story_id,
-              }: any) => {
-                if (
-                  !story_title ||
-                  !story_url ||
-                  !created_at ||
-                  !comment_text
-                ) {
-                  return null;
-                } else {
-                  return (
-                    <InfoCard
-                      prevLocalStorageState={favoritePosts}
-                      headerText={story_title}
-                      centralText={comment_text}
-                      headerIcon={<TimeSVG />}
-                      storyUrl={story_url}
-                      localStorageSetter={setFavoritePosts}
-                      id={story_id}
-                      query={data.query}
-                      selectedDropdown={selectedDropdown}
-                    />
-                  );
+            <div>
+              {data.hits.map(
+                ({
+                  story_title,
+                  story_url,
+                  created_at,
+                  comment_text,
+                  story_id,
+                }: any) => {
+                  if (
+                    !story_title ||
+                    !story_url ||
+                    !created_at ||
+                    !comment_text
+                  ) {
+                    return null;
+                  } else {
+                    return (
+                      <InfoCard
+                        prevLocalStorageState={favoritePosts}
+                        headerText={story_title}
+                        centralText={comment_text}
+                        headerIcon={<TimeSVG />}
+                        storyUrl={story_url}
+                        localStorageSetter={setFavoritePosts}
+                        id={story_id}
+                        query={data.query}
+                        selectedDropdown={selectedDropdown}
+                      />
+                    );
+                  }
                 }
-              }
-            )}
-            <div>Current Page: {page + 1}</div>
-            <button
-              onClick={() => setPage((old) => Math.max(old - 1, 0))}
-              disabled={page === 0}
-            >
-              Previous Page
-            </button>
-            <button
-              onClick={() => {
-                setPage(page + 1);
-              }}
-              disabled={isPreviousData}
-            >
-              Next Page
-            </button>
+              )}
+            </div>
+            <div>
+              <div>Current Page: {page + 1}</div>
+              <button
+                onClick={() => setPage((old) => Math.max(old - 1, 0))}
+                disabled={page === 0}
+              >
+                Previous Page
+              </button>
+              <button
+                onClick={() => {
+                  setPage(page + 1);
+                }}
+                disabled={isPreviousData}
+              >
+                Next Page
+              </button>
+            </div>
           </div>
         )}
 
