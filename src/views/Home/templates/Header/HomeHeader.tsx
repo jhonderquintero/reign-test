cbc420fe9a1e641221097e5372ec6f0d776de68a
Jhonder Quintero <jhonderquintero@gmail.com>
2022-01-03 08:29:10 -0400

    responsive grids

--- src/views/Home/templates/Header/HomeHeader.tsx
+++ src/views/Home/templates/Header/HomeHeader.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { IHomeHeader } from "./definitions";
-import "./styles.css"
+import "./styles.css";
 
 export const HomeHeader: React.FC<IHomeHeader> = ({
   LeftElement,
-  CenterElement,
-  RightElement,
 }): JSX.Element => {
   return (
     <header className="header">
-      {LeftElement}
-      {/* <div className="header__center-element">{CenterElement}</div>
-      <div className="header__right-element">{RightElement}</div> */}
+      <div>{LeftElement}</div>
     </header>
   );
 };
